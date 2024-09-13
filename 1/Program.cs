2018ef8749b7859b58e2caf94b5344056f4f7b89
mka <mka@A34PC14>
2024-09-13 12:32:11 +0300

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("Happy");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("birthday");
+
+Console.WriteLine("Happy birthday");
+
+
